1618ed44bf96d473a6e61ae64c06c8633ad56634
jlightyear <joseluis.estrach@gmail.com>
2015-05-01 19:13:16 +0200

    Added methods and paths to upload images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,37 @@
 class Image < ActiveRecord::Base
   belongs_to :product
+
+  after_save :save_image
+
+  PATH_IMGFILES = File.join Rails.root, "public", "images"
+
+  def imgfile = (imgfile)
+    unless imgfile.blank?
+      @imgfile = imgfile
+      self.name = imgfile.original_filename
+      self.extension = imgfile.original_filename.split(".").last.downcase
+    end
+  end
+
+  def path_image
+    File.join PATH_IMGFILES, "#{self.id}.#{self.extension}"
+  end
+
+  def have_file?
+    File.exists? path_image
+  end
+
+
+  private
+
+  def save_image
+    if @imgfile
+      FileUtils.mkdir_p PATH_IMGFILES
+      File.open(path_image, "wb") do |f|
+        f.write(@imgfile.read)
+      end
+      @imgfile = nil
+    end
+  end
+
 end
